11ff13d140236e88257672704a65d71e83d9c2c2
Oleh Malakan <130376724+oleh-malakan@users.noreply.github.com>
2025-05-18 06:09:46 +0300

    Delete file.h

--- file.h
+++
@@ -1,9 +0,0 @@
-T (
-)
-  
-Open(path []) (t * T)
-Read(t * T, b []) (n uint, err)
-Write(t * T, b []) (n uint, err)
-ReadAt(t * T, b [], offset uint) (n uint, err)
-WriteAt(t * T, b [], offset uint) (n uint, err)
-Free(t * T)
