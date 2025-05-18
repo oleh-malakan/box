df2c18441ac3a981f3274841fd89bdc695679f62
Oleh Malakan <130376724+oleh-malakan@users.noreply.github.com>
2025-05-18 06:06:09 +0300

    Update file.h

--- file.h
+++ file.h
@@ -2,4 +2,8 @@ T (
 )
   
 Open(path []) (t * T)
+Read(t * T, b []) (n uint, err)
 Write(t * T, b []) (n uint, err)
+ReadAt(t * T, b [], offset uint) (n uint, err)
+WriteAt(t * T, b [], offset uint) (n uint, err)
+Free(t * T)
